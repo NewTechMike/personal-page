d9c5fb3a6a4cfa5b193fd1a43be740fdd403ab6e
Michael Harris <xero5k@yahoo.com>
2021-07-11 21:40:58 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const header = document.getElementById('header');
+header.addEventListener('mouseover', function(){
+    alert('m hovered over');
+});
+
+//document.querySelector('body').appendChild('h1');
